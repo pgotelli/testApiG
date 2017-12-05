00fe2e18765ae7917533c30d0309790dec212fe9
pgotelli <pgotelli@voiceovernetinc.com>
2017-12-05 16:47:59 -0300

    Signed-off-by: pgotelli <pgotelli@voiceovernetinc.com>

--- src/app/login-comp/login-comp.component.ts
+++ src/app/login-comp/login-comp.component.ts
@@ -10,6 +10,8 @@ import * as AWS from "aws-sdk/global";
 import * as awsservice from "aws-sdk/lib/service";
 import * as CognitoIdentity from "aws-sdk/clients/cognitoidentity";
 
+import { HttpClient, HttpHeaders, HttpErrorResponse } from '@angular/common/http';
+
 //import * as apigClientFactory from "assets/apigwsdk/apigClient.js";
 
 //declare var apigClientFactory : any;
@@ -23,11 +25,55 @@ import * as CognitoIdentity from "aws-sdk/clients/cognitoidentity";
 })
 export class LoginCompComponent implements OnInit {
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
 
   ngOnInit() {
     console.log("first console log", apigClientFactory);
     let something = apigClientFactory.newClient();
+
+    let job = {
+       "projectId": "P-RDR178",
+       "externalId": "JOB10",
+       "customerName": "JOB10",
+       "status": "JOB_NEW",
+       "requestedDueDate":1509552000000,
+       "currentDueDate":1509552000000,
+       "actualDuration": 1800000,
+       "estimatedDuration": 1800000,
+//            "latitude": -34.9137939,
+//            "longitude": -57.9934855,
+//            "address": "Estadio Ciudad de La Plata",
+//            "city": "La Plata",
+//            "state": "Buenos Aires",
+//            "zipCode": "1900"
+        /*"assignee": {
+          "userName": "rmtest330",
+          "userId": "cognito-idp.us-east-1.amazonaws.com:us-east-1_GFaGz2chu:49b2b964-116e-4467-a65e-602541498f03"
+        },*/
+        "taskDefinitions" : ["P-RDR178_$_1557c708-d334-4972-b2d0-56e52ba8d39e"]
+    }
+
+    this.http.post('https://aoa0zvw5v2.execute-api.us-east-1.amazonaws.com/test/externaljobs', job, {
+      headers: new HttpHeaders().set('Authorization', 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiJjb2duaXRvLWlkcC51cy1lYXN0LTEuYW1hem9uYXdzLmNvbTp1cy1lYXN0LTFfR0ZhR3oyY2h1OmFmMWY0ZGU1LThkYzEtNDFiNy1hMGU2LTA1NGFiN2E3YTI2NiJ9.46410HtUYkF4AcnfQSXCIwxt6erST-KqfmoTouN1zSo'),
+    })
+    .subscribe(data => {
+      console.log(data);
+    },
+    (err: HttpErrorResponse) => {
+        if (err.error instanceof Error) {
+          // A client-side or network error occurred. Handle it accordingly.
+          console.log('An error occurred:', err.error.message);
+        } else {
+          // The backend returned an unsuccessful response code.
+          // The response body may contain clues as to what went wrong,
+          console.log(`Backend returned code ${err.status}, body was: ${err.error}`);
+        }
+      });
+    /*something = true;
+    if(something) {
+      return;
+    }*/
+
     var awsConfig = {
       region: 'us-east-1',
       IdentityPoolId: 'us-east-1:87b000cc-4720-41e6-a422-aa4d3a5ed544',
@@ -139,7 +185,22 @@ export class LoginCompComponent implements OnInit {
                     },*/
                     "taskDefinitions" : ["P-RDR178_$_1557c708-d334-4972-b2d0-56e52ba8d39e"]
                 }
+                /*
                 apigClient.externaljobsPost('', job, '').then((response) => {
+
+                  console.log("se encontro la data buscada", response);
+
+                }).catch(err => {
+
+                    console.log('error', err);
+
+                });*/
+
+                let search = {
+                  "jobId": "culo"
+                }
+
+                apigClient.taskGet(search, '', '').then((response) => {
 
                   console.log("se encontro la data buscada", response);
 
